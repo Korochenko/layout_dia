32b0883d271d5c12f9a0f84359fa065f5c9a1601
Kostya <kostantinkorocenko301@gmaol.com>
2026-04-01 15:35:25 +0300

    checklist changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const form = document.querySelector('.footer__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  form.reset();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+const slider = [
+  '/src/images/slider/header_background.png',
+  '/src/images/slider/slide-img-1.jpg',
+  '/src/images/slider/slide-img-2.jpg',
+];
+
+let current = 0;
+
+const bg = document.querySelector('.header__bottom');
+const [btnLeft, btnRight] = document.querySelectorAll('.slider__button');
+function setSlide(index) {
+  bg.style.backgroundImage = `url(${slider[index]})`;
+}
+
+setSlide(0);
+
+btnLeft.addEventListener('click', () => {
+  current = (current - 1 + slider.length) % slider.length;
+  setSlide(current);
+});
+
+btnRight.addEventListener('click', () => {
+  current = (current + 1) % slider.length;
+  setSlide(current);
+});
+
+
